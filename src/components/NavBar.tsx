b7e49b057f0e8612c32811b6522a051fdecb4fd2
Kenneth Camacho <Kennethcxv@gmail.com>
2026-03-22 15:15:28 -0400

    Added NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,23 @@
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Box from "@mui/material/Box";
+
 const NavBar = () => {
   return (
     <>
-    <h1>awd</h1>
+    <AppBar position="fixed">
+      <Toolbar >
+        <Box sx={{ display:"flex" ,gap:5}}>
+        <Typography sx={{"&:hover": {opacity: 0.8,},cursor:"pointer"}} variant="h6" component="a" href="/products">Products</Typography>
+        <Typography sx={{"&:hover": {opacity: 0.8,},cursor:"pointer"}} variant="h6" component="a" href="/contact">Contact</Typography>
+        <Typography sx={{"&:hover": {opacity: 0.8,},cursor:"pointer"}} variant="h6" component="a" href="/cart">Cart</Typography>
+        <Typography sx={{"&:hover": {opacity: 0.8,},cursor:"pointer"}} variant="h6" component="a" href="/checkout">Checkout</Typography>
+        </Box>
+      </Toolbar>
+
+    </AppBar>
+    <Toolbar />
     </>
     )
 }
